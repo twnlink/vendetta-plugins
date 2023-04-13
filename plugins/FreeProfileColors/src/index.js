0f80ce878d2b2137c6ac46c056b2459eebf8c03d
toonlink <toonlink@riseup.net>
2023-04-13 16:43:53 -0400

    fix the regex and removal

--- plugins/FreeProfileColors/src/index.js
+++ plugins/FreeProfileColors/src/index.js
@@ -3,7 +3,8 @@ import { after } from "@vendetta/patcher";
 import { decode3y3, is3y3 } from "./3y3";
 
 const patches = [];
-const colorRegex = /.*\[(\#[0-9a-fA-F]{6})\s*,\s*(\#[0-9a-fA-F]{6})\].*/;
+const colorRegex = /\[(\#[0-9a-fA-F]{6})\s*,\s*(\#[0-9a-fA-F]{6})\]/;
+const globalColorRegex = new RegExp(colorRegex, "g");
 
 const UserProfileStore = findByStoreName("UserProfileStore");
 
@@ -24,7 +25,7 @@ export function onLoad() {
 
         resp.themeColors = colors.map((c) => parseInt("0x" + c.slice(1)));
         resp.premiumType = 2;
-        resp.bio = decoded.replaceAll(colorRegex, "");
+        resp.bio = decoded.replaceAll(globalColorRegex, "");
       } catch {}
     })
   );
